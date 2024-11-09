9c14880f47a91170fd2d7d89d5e0b5324f706df8
Aanya Shukla <aanyashukla67@gmail.com>
2024-11-09 14:55:24 +0000

    Learnt about Mapping data to Components.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,8 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
+        <p>{props.id}</p>{" "}
+        {/*key is not something that we can tap into, so we have have to create our custom prop hence used prop named id*/}
         <h2 className="name">{props.name}</h2>
         <Avatar img={props.img} />
       </div>
