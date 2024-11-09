85773dd1a6b4891074a45ba36b25dfb705730260
Aanya Shukla <aanyashukla67@gmail.com>
2024-11-09 21:08:26 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
-        <p>{props.id}</p>{" "}
+        <p>{props.id}</p>
         {/*key is not something that we can tap into, so we have have to create our custom prop hence used prop named id*/}
         <h2 className="name">{props.name}</h2>
         <Avatar img={props.img} />
